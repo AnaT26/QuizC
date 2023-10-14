be043ae2fcba8a3443e4d9e42c6f95d81efd549a
Ana Laura Tostes <68925429+AnaT26@users.noreply.github.com>
2023-10-14 02:10:22 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+      question: "Qual foi o primeiro filme que a gente assistiu juntas?",
+      answers: [
+        { option: "Jogos Vorazes", correct: true },
+        { option: "A freira", correct: false },
+        { option: "AnaVitoria", correct: false },
+      ],
+    },
+    {
+      question: "Qual foi nosso priemeiro date?",
+      answers: [
+        { option: "Jantar", correct: false },
+        { option: "Cinema", correct: true },
+        { option: "Pizzada", correct: false },
+      ],
+    },
+    {
+      question: "Onde foi nosso priemeiro beijo?",
+      answers: [
+        { option: "Cobal", correct: false },
+        { option: "Calourada", correct: false },
+        { option: "Pizzada", correct: true },
+      ],
+    },
+    {
+      question: "Qual Musica eu dediquei pra você?",
+      answers: [
+        { option: "Idiota", correct: false },
+        { option: "Trevo", correct: false },
+        { option: "Labia", correct: true },
+      ],
+    },
+  ];
